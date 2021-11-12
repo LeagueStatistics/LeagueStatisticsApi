d02f463032442c0f105f4eae072111f8d23fd083
Luis Henrique Nicoleli de Mello <lhnmello@hotmail.com>
2021-11-12 07:05:37 -0300

    Classes Account, Champion e ChampionMastery

---
+++ LeagueStatistics/LeagueStatistics.Domain/Champion/ChampionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeagueStatistics.Domain.Champion
+{
+    public class ChampionInfo
+    {
+        public int MaxNewPlayerLevel { get; set; }
+        public List<int> FreeChampionIdsForNewPlayers { get; set; }
+        public List<int> FreeChampionIds { get; set; }
+    }
+}
--- LeagueStatistics/LeagueStatistics.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LeagueStatistics.Domain
-{
-    public class Class1
-    {
-    }
-}
